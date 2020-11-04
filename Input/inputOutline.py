a000734998452ebe4eb2ee235985353d415cf806
Yazmin Favela <yazmin.favela92@gmail.com>
2020-11-03 18:44:51 -0600

    input outline

---
+++ Input/inputOutline.py
@@ -0,0 +1,25 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Get input from the user to the question "What is your favorite color?"
+#and store their answer in a variable called favColor.
+
+userResponse = input ("what is your favorite color?")
+favColor = "Green" 
+
+#2) Make a variable called question and set it to "What is your name?". Then
+#get input from the user to the question and store their answer in a variable
+#called name.
+
+question =  input ("what is your name?")
+name = "Yazmin"
+
+#3) Get input from the user to the question "What is your age?" and store their
+#answer in a variable called age. Convert age from a string variable to a int
+#(integer) variable.
+
+user = input("what is your age?")
+age = "16"
+age = 16
